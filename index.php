c76bf8939e47db2f62686e1f4c654d265b64d542
ravenusmc <mcuddy77@gmail.com>
2020-07-13 06:33:55 -0400

    started work on login page

--- index.php
+++ index.php
@@ -1,2 +1,42 @@
 <?php
+
+  session_start();
+  //Pulling in the databases
+  // require('./model/database.php');
+  // require('./model/helpers.php');
+  //
+  // global $db;
+  // $message = "";
+
 ?>
+
+<?php include 'view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+<!-- CSS for the header -->
+<link rel="stylesheet" type="text/css" href="./assets/css/header.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/login.css">
+
+<main>
+
+  <!-- Start of error handling -->
+  <?php
+    if (isset($message)){
+      echo $message;
+    }
+  ?>
+  <!-- End of error handling -->
+
+  <form method="post" class='form'>
+
+    <div class='form-item'>
+      <input name='username' type='text' class='form-input' placeholder="Username" aria-label="Username">
+    </div>
+
+    <div class='form-item'>
+      <input name='password' type='password' class='form-input' placeholder="Password" aria-label="Password">
+    </div>
+
+    <button class='form-button' type='submit'>Submit</button>
+  </form>
+
+</main>
---
+++ view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+  <title>Blog Ratings</title>
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- Jquery -->
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+<body>

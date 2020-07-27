6122223e9c50115e4e8cd780264a7959f032b2bc
ravenusmc <mcuddy77@gmail.com>
2020-07-27 16:37:25 -0400

    settting up model for the garden beds

---
+++ model/Gardens.php
@@ -0,0 +1,21 @@
+<?php
+
+  class Gardens {
+
+    private $bedID, $bed, $plantID, $location, $timePeriod, $plantDate, $firstPickDate, $lastPickDate;
+
+    public function __construct() {
+     $this->bedID = 0;
+     $this->bed = '';
+     $this->plantID = '';
+     $this->location = '';
+     $this->timePeriod = '';
+     $this->plantDate = '';
+     $this->firstPickDate = '';
+     $this->lastPickDate = '';
+   }
+
+
+  }
+
+?>

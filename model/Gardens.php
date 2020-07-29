d4d01899879d1681e75bae18a88f0ee96a933d9f
ravenusmc <mcuddy77@gmail.com>
2020-07-29 08:12:40 -0400

    set up class for gardens

--- model/Gardens.php
+++ model/Gardens.php
@@ -5,17 +5,79 @@
     private $bedID, $bed, $plantID, $location, $timePeriod, $plantDate, $firstPickDate, $lastPickDate;
 
     public function __construct() {
-     $this->bedID = 0;
-     $this->bed = '';
-     $this->plantID = '';
-     $this->location = '';
-     $this->timePeriod = '';
-     $this->plantDate = '';
-     $this->firstPickDate = '';
-     $this->lastPickDate = '';
-   }
+       $this->bedID = 0;
+       $this->bed = '';
+       $this->plantID = '';
+       $this->location = '';
+       $this->timePeriod = '';
+       $this->plantDate = '';
+       $this->firstPickDate = '';
+       $this->lastPickDate = '';
+    }
 
+    public function getBedID() {
+         return $this->bedID;
+    }
+
+    public function setBedID($value) {
+          $this->bedID = $value;
+    }
+
+    public function getbed() {
+         return $this->bed;
+    }
+
+    public function setbed($value) {
+          $this->bed = $value;
+    }
+
+    public function getplantID() {
+         return $this->plantID;
+    }
+
+    public function setplantID($value) {
+          $this->plantID = $value;
+    }
+
+    public function getlocation() {
+         return $this->location;
+    }
+
+    public function setlocation($value) {
+          $this->location = $value;
+    }
+
+    public function gettimePeriod() {
+         return $this->timePeriod;
+    }
+
+    public function settimePeriod($value) {
+          $this->timePeriod = $value;
+    }
+
+    public function getplantDate() {
+         return $this->plantDate;
+    }
+
+    public function setplantDate($value) {
+          $this->plantDate = $value;
+    }
+
+    public function getfirstPickDate() {
+         return $this->firstPickDate;
+    }
+
+    public function setfirstPickDate($value) {
+          $this->firstPickDate = $value;
+    }
+
+    public function getlastPickDate() {
+         return $this->lastPickDate;
+    }
+
+    public function setlastPickDate($value) {
+          $this->lastPickDate = $value;
+    }
 
   }
-
 ?>
